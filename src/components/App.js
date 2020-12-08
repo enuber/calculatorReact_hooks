c10f07789843f458cd391ae7155ea854ad65be4c
Erik Nuber <erik.nuber@yahoo.com>
2020-12-08 14:45:20 -0800

    updated title and added in homepage for build

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Calculator from '../layout/Calculator';
 const app = () => {
     return(
         <div className="app">
+            <h1 style={{textAlign: "center"}}>Calculator Using React With Hooks</h1><br/>
             <Calculator/>
         </div>
     )
